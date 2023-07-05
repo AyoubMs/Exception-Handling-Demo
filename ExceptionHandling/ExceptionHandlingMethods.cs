5d5b2cb118b61107d255e94bc65d1e69fe4c176d
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:49:51 +0200

    add app

---
+++ ExceptionHandling/ExceptionHandlingMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class ExceptionHandlingMethods
+    {
+        public static void BadCall(int[] ages)
+        {
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("We had an error");
+                    Console.WriteLine(ex.Message);
+                    throw new Exception("There was an error handling our array", ex);
+                }
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using ExceptionHandling;
+
+int[] ages = new int[] { 1, 3, 5 };
+
+try
+{
+    ExceptionHandlingMethods.BadCall(ages);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("There was an exception thrown in our app");
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadLine();
